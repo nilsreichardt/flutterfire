6e51483ad8913ef33dd099d5855c6788777196bf
nilsreichardt <nils@reichardt.io>
2022-06-13 11:33:29 +0200

    Remove `setupTests`

--- tests/integration_test/integration_e2e_test.dart
+++ tests/integration_test/integration_e2e_test.dart
@@ -21,25 +21,6 @@ import 'firebase_core/firebase_core_e2e_test.dart' as firebase_core;
 //     as firebase_remote_config;
 // import 'firebase_storage/firebase_storage_e2e.dart' as firebase_storage;
 
-void setupTests() {
-  // Core first.
-  // firebase_core.setupTests();
-  // All other tests.
-  // firebase_auth.setupTests();
-  // cloud_functions.setupTests();
-  // firebase_storage.setupTests();
-  // firebase_database.setupTests();
-  // firebase_app_check.setupTests();
-  // firebase_messaging.setupTests();
-  // firebase_analytics.setupTests();
-  // firebase_crashlytics.setupTests();
-  // firebase_dynamic_links.setupTests();
-  // firebase_remote_config.setupTests();
-  // firebase_in_app_messaging.setupTests();
-  // firebase_app_installations.setupTests();
-  // firebase_ml_model_downloader.setupTests();
-}
-
 void main() {
   group('FlutterFire', () {
     firebase_core.main();
